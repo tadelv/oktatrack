d6dc1b0de4058c643cbb410c983f467a0cbb781d
Vid Tadel <tadelv@gmail.com>
2023-01-18 13:13:41 +0100

    use modern app definition

---
+++ OktaTrack/OktaTrack.swift
@@ -0,0 +1,18 @@
+//
+//  OktaTrack.swift
+//  OktaTrack
+//
+//  Created by Vid Tadel on 1/18/23.
+//  Copyright © 2023 Delta96. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct OktaTrack: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
