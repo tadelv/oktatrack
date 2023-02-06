772c636c86d61c60549b4c310a2dcd3cbd31ccd4
Vid Tadel <tadelv@gmail.com>
2023-02-06 08:13:51 +0100

    add Destination which doesn't require Equatable

--- OktatrackLib/Sources/Helpers/ValueNavigation.swift
+++ OktatrackLib/Sources/Helpers/ValueNavigation.swift
@@ -15,6 +15,14 @@ extension View {
     self
       .modifier(ValueNavigationDestination(value: value, destination: destination))
   }
+
+  public func navigateIfValue<Value, Destination: View>(
+    _ value: Binding<Value?>,
+    destination: @escaping (Value) -> Destination
+  ) -> some View {
+    self
+      .modifier(NonEquatableDestination(value: value, destination: destination))
+  }
 }
 
 struct ValueNavigationDestination<Value: Equatable, Destination: View>: ViewModifier {
@@ -38,6 +46,26 @@ struct ValueNavigationDestination<Value: Equatable, Destination: View>: ViewModi
         shows = newValue != nil
       })
       .navigationDestination(isPresented: $shows) {
+        if let value {
+          destination(value)
+        }
+      }
+  }
+}
+
+struct NonEquatableDestination<Value, Destination: View>: ViewModifier {
+  @Binding var value: Value?
+  @ViewBuilder var destination: (Value) -> Destination
+
+  func body(content: Content) -> some View {
+    content
+      .navigationDestination(isPresented: .init(get: {
+        value != nil
+      }, set: { showing, transaction in
+        if !showing {
+          $value.transaction(transaction).wrappedValue = nil
+        }
+      })) {
         if let value {
           destination(value)
         }
